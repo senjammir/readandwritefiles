b48b0179a85d91bda9355904afefb446f492b71e
SenJamir <aosen.xeno@gmail.com>
2023-01-30 23:55:44 -0600

    changes to customer country

--- customer_country.py
+++ customer_country.py
@@ -3,12 +3,13 @@
 
 customer_infile = open("customers.csv", "r")
 customer_outfile = open("customer_country.csv", "w")
+customer_outfile.write("Customer Name, Country\n")
+count = 0
+for i in customer_infile.readlines()[1:]:
+    x = i.split(",")
+    customer_outfile.write(f"{x[2]} {x[1]}, {x[4]}\n")
+    count += 1
 
-for i in range(92):  # for loop to iterate over the 92 lines in the file
-    x = customer_infile.readline()  # convert line into string
-    x = x.split(",")  # convert string into list
-    customer_outfile.write(
-        f"{x[1]},{x[2]},{x[4]}\n"
-    )  # write the desired fields into new file
+print("Total number of customers:", count)
+
 customer_outfile.close()
-
